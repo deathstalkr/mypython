68dd039435937197ec7198cbe5928c01bdcaad5b
SOLOMON JOHNSON <36148681+Solomon98@users.noreply.github.com>
2018-06-20 23:12:11 +0530

    Update sp.py

--- sp.py
+++ sp.py
@@ -1,9 +1,9 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 
-harvard = sr.AudioFile("harvard.wav")
+harvard = sr.AudioFile("harvard.wav")     #you can download the audio file from this same repository
 with harvard as source:
     r.adjust_for_ambient_noise(source)
     audio = r.record(source)
-out=r.recognize_sphinx(audio)
+out=r.recognize_google(audio)
 print(out)
