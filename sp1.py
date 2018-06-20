dc62fc29f07d019ae1b7a7449151941719f1ac69
SOLOMON JOHNSON <36148681+Solomon98@users.noreply.github.com>
2018-06-20 23:03:39 +0530

    Update sp1.py

--- sp1.py
+++ sp1.py
@@ -1,9 +1,9 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 
-jackhammer = sr.AudioFile("jackhammer.wav")
+jackhammer = sr.AudioFile("jackhammer.wav")    #jachammer.wav is an audio file with background noise of jackhmmer
 with jackhammer as source:
-    r.adjust_for_ambient_noise(source,duration=0.5)
+    r.adjust_for_ambient_noise(source,duration=0.5)  #it removes unwanted noise.....duration is set to 1 by default
     audio = r.record(source)
-out=r.recognize_sphinx(audio,show_all=True)
+out=r.recognize_google(audio,show_all=True)  #in place of google u cwn replace it with other suppotred API's
 print(out)
