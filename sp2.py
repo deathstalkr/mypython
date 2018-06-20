aee3c1a9cac8645a9b7f0d008e85f043b4b136ad
SOLOMON JOHNSON <36148681+Solomon98@users.noreply.github.com>
2018-06-20 23:09:00 +0530

    Update sp2.py

--- sp2.py
+++ sp2.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
-import pyaudio
+import pyaudio   #optional
 
 # get audio from the microphone
-while True:
+while True:                     #this loop runs the below code infinite times until any inturrupt is generated 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
@@ -10,13 +10,13 @@ while True:
         audio = r.listen(source)
 
     try:
-        output= r.recognize_google(audio,language='en')
+        output= r.recognize_google(audio,language='en')  #can change language to your desired language 
         print("You said: " +output)
     except sr.UnknownValueError:
         print("Could not understand audio")
     except sr.RequestError as e:
         print("Could not request results.Please check your internet connection and try again; {0}".format(e))
-    if output=='exit':
+    if output=='exit':     #if the input voice is matched with the passed argument the loop terminates 
         break
-    else:
+    else:                  #loop continues to run
         continue
