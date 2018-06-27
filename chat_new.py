9e66dea98ac2fa84ff76009ee80d33243711e218
SOLOMON JOHNSON <solomon.johnson098@gmail.com>
2018-06-28 02:14:54 +0530

    Add files via upload
    perfect Dictionary bot (Updated)

--- chat_new.py
+++ chat_new.py
@@ -23,30 +23,34 @@ while True:
     if output=='close':
         break
     else:
-        out_come = wordnet.synsets(output)
-        # print data
-        #print(out_come)
+        try:
+            out_come = wordnet.synsets(output)
+            # print data
+            #print(out_come)
 
-        def exception_handler(exception_type, exception, traceback):
-                # All your trace are belong to us!
+            def exception_handler(exception_type, exception, traceback):
                 # your format
-            print() # to track the error u can put ("%s: %s" % (exception_type.__name__, exception)) inside print
-        sys.excepthook = exception_handler
+                print() # to track the error u can put ("%s: %s" % (exception_type.__name__, exception)) inside print
+            sys.excepthook = exception_handler
 
-        # definition
-        language='en'
-        sp=out_come[0].definition()
-        '''
-        print("\n 1. noun: ", sp)
-        print('Expamples: ', out_come[0].examples())
+            # definition
+            language='en'
+            sp=out_come[0].definition()
 
-        print('\n 2. adg:  ', out_come[1].definition())
-        print('Expamples: ', out_come[1].examples())
+            print("\n 1. noun: ", sp)
+            print('Expamples: ', out_come[0].examples())
+            '''
+            print('\n 2. adg:  ', out_come[1].definition())
+            print('Expamples: ', out_come[1].examples())
 
-        print('\n 3. adv:  ', out_come[2].definition())
-        print('Expamples: ', out_come[2].examples())
-        '''
-        myobj=gTTS(text=sp,lang=language,slow=False)
-        myobj.save('word.mp3')
-        os.system("cvlc --play-and-exit word.mp3")
-        os.remove('word.mp3')
+            print('\n 3. adv:  ', out_come[2].definition())
+            print('Expamples: ', out_come[2].examples())
+            '''
+            myobj=gTTS(text=sp,lang=language,slow=False)
+            myobj.save('word.mp3')
+            os.system("cvlc --play-and-exit word.mp3")
+            os.remove('word.mp3')
+
+        except IndexError:
+            print('Sorry!! Word could not be found in the dictionary')
+            continue
